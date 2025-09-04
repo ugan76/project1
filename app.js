c5ab7b81b3cf671752e3e0b8e0f13e7c1dae24f9
ugan76 <47847745+ugan76@users.noreply.github.com>
2025-09-04 12:50:36 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from PaaS - CapRover!');
+}).listen(port);
