328a255a4e692e1c72ef203701ed0b866c18e0fc
ugan76 <47847745+ugan76@users.noreply.github.com>
2025-09-18 11:23:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from PaaS - CapRover!');
-}).listen(port);
+  res.end('Hello from CapRover!');
+}).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
