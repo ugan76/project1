ee287965aebf32d73882f5014e9e631d89c82976
ugan76 <47847745+ugan76@users.noreply.github.com>
2025-09-18 12:54:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from CapRover!');
-}).listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from CapRover Node.js app!\n');
+}).listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
